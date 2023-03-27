264a9e3008815abc6240c83a813c0d30af54d957
Shams Parvez Arka <parvez6826@gmail.com>
2023-03-27 20:46:44 +0600

    main installation script

--- install.sh
+++ install.sh
@@ -8,3 +8,13 @@ cp init.lua $HOME/.config/nvim/
 cp -R colors $HOME/.config/nvim/ 
 cp -R lua $HOME/.config/nvim/
 cp -R plugin $HOME/.config/nvim/ 
+
+# Install zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+# Bootstrap zsh
+cp .zshrc $HOME/.zshrc
+source ~/.zshrc
