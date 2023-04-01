908ebd162fa7dbf0718980e7f004db8c106374bc
Shams Parvez Arka <parvez6826@gmail.com>
2023-04-01 16:47:43 +0600

    bootstraping lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,9 +1,26 @@
-local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = true,
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
-})
+local lspkind = require("lspkind")
+require'cmp'.setup {
+	formatting = {
+		format = lspkind.cmp_format({
+			wirth_text = false,
+			mode = 'text_symbol',
+			max_width = 50,
+			ellipsis_char = '...',
+			preset = 'default'
+		})
+	},
+	sources = {
+		{ name = 'nvim_lsp' }
+	}
+}
 
-lsp.nvim_workspace()
-lsp.setup()
+local lspconfig = require("lspconfig")
+lspconfig.clangd.setup {}
+lspconfig.cssls.setup {}
+lspconfig.html.setup {}
+lspconfig.ltex.setup {}
+lspconfig.lua_ls.setup {}
+lspconfig.marksman.setup {}
+lspconfig.pyright.setup {}
+lspconfig.tsserver.setup {}
+
