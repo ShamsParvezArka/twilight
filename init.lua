0977ba7579d41f48caa72b346da9562d529f5401
Shams Parvez Arka <parvez6826@gmail.com>
2023-04-04 17:59:38 +0600

    bootstrap everything in neovim startup

--- init.lua
+++ init.lua
@@ -1,17 +1,27 @@
-require('base')
-require('plugins')
-require('lsp')
+require("base")
+require("plugins")
+require("lsp")
 
--- Start status line
-require('lualine').setup{
-	options = {
-		component_separators = { left = '', right = ''},
-		section_separators = { left = '', right = ''},
-		theme = 'ayu_mirage'
-	}
-}
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+require("mason-lspconfig").setup()
+require("lspkind")
 
--- Start treesitter
+-- require('lualine').setup{
+-- 	options = {
+-- 		component_separators = { left = '', right = ''},
+-- 		section_separators = { left = '', right = ''},
+-- 		theme = 'ayu_mirage'
+-- 	}
+-- }
+
 require 'nvim-treesitter.configs'.setup{
 	ensure_installed = { "c", "cpp", "python", "lua", "vim", "javascript", "bash", "help", "query", "markdown", "markdown_inline"},
 	sync_install = true,
@@ -23,34 +33,3 @@ require 'nvim-treesitter.configs'.setup{
 	additional_vim_regex_highlighting = true,
 }
 
--- Start smooth cursor
-require('smoothcursor').setup({
-	autostart = true,
-	cursor = "◽",             -- cursor shape (need nerd font)
-	texthl = "SmoothCursor",   -- highlight group, default is { bg = nil, fg = "#FFD400" }
-	linehl = nil,              -- highlight sub-cursor line like 'cursorline', "CursorLine" recommended
-	type = "default",          -- define cursor movement calculate function, "default" or "exp" (exponential).
-	fancy = {
-		enable = false,        -- enable fancy mode
-		head = { cursor = "◽", texthl = "SmoothCursor", linehl = nil },
-		body = {
-			{ cursor = "", texthl = "SmoothCursorRed" },
-			{ cursor = "", texthl = "SmoothCursorOrange" },
-			{ cursor = "●", texthl = "SmoothCursorYellow" },
-			{ cursor = "●", texthl = "SmoothCursorGreen" },
-			{ cursor = "•", texthl = "SmoothCursorAqua" },
-			{ cursor = ".", texthl = "SmoothCursorBlue" },
-			{ cursor = ".", texthl = "SmoothCursorPurple" },
-		},
-		tail = { cursor = nil, texthl = "SmoothCursor" }
-	},
-	flyin_effect = nil,        -- "bottom" or "top"
-	speed = 100,                -- max is 100 to stick to your current position
-	intervals = 35,            -- tick interval
-	priority = 10,             -- set marker priority
-	timeout = 3000,            -- timout for animation
-	threshold = 3,             -- animate if threshold lines jump
-	disable_float_win = false, -- disable on float window
-	enabled_filetypes = nil,   -- example: { "lua", "vim" }
-	disabled_filetypes = nil,  -- this option will be skipped if enabled_filetypes is set. example: {"TelescopePrompt","NvimTree" }
-})
